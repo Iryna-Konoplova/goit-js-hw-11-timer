99e3c122dec5b65fea9cbe1499d7e6351e80a87e
Iryna Konoplova <a.irina773@gmail.com>
2021-05-15 01:06:39 +0300

    Запускает таймер

--- timer.js
+++ timer.js
@@ -18,25 +18,41 @@ class CountdownTimer {
   start() {
     setInterval(() => {
       const currentTime = Date.now();
-      const deltaTime = this.targetDate - currentTime;
-      const time = getTimeComponents(deltaTime);
-      // this.onTick(time);
-      updateClockface(time);
+      this.updateClockface(currentTime);
+      // const deltaTime = this.targetDate - currentTime;
+      // const time = getTimeComponents(deltaTime);
+      // // this.onTick(time);
+      // updateClockface(time);
     }, 1000);
   } 
   
- 
-
-    pad(value) {
+  pad(value) {
       return String(value).padStart(2, '0');
-    }
+  }
 
-    updateClockface(time) {
+  updateClockface(currentTime) {
+      const time = this.targetDate - currentTime;
       this.fieldDays.textContent = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
       this.fieldHours.textContent = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
       this.fieldMins.textContent = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
       this.fieldSecs.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-}
+  }
+  
+//   getTimeComponents(time) {
+//   const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+//   const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+//   const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//   const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+//   return { days, hours, mins, secs };
+//   }
+  
+//   updateClockface({ days, hours, mins, secs }) {
+//   this.fieldDays.textContent = `${days}`;
+//   this.fieldHours.textContent = `${hours}`;
+//   this.fieldMins.textContent = `${mins}`;
+//   this.fieldSecs.textContent = `${secs}`;
+// }
 
 
 }
@@ -45,92 +61,4 @@ const myCountdownTimer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jul 17, 2021'),
 });
-
-
-
-
-
-// class CountdownTimer {
-//   constructor({ onTick }) {
-//     this.onTick = onTick;
-//    }
-//   start() {
-//     const finishTime = new Date('Jul 17, 2021').getTime();
-    
-//     setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTime = finishTime - currentTime;
-//       const time = getTimeComponents(deltaTime);
-//       this.onTick(time);
-//       // updateClockface(time);
-//     }, 1000);
-//   }
-  
-// }
-
-
-// const countdownTimer = new CountdownTimer({
-//   onTick: updateClockface,
-// });
-  
-// console.log(countdownTimer)
-
-
-
-
-
-
-
-
-
-
-
-// const CountdownTimer = {
-//   start() {
-//     const finishTime = new Date('Jul 17, 2021').getTime();
-//     console.log(finishTime);
-//     // const startTime = Date.now('time');
-//     // console.log(startTime);
-
-//     setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTime = finishTime - currentTime;
-//       const time = getTimeComponents(deltaTime);
-//       updateClockface(time);
-//       // console.log(`${days}:${hours}:${mins}:${secs}`);
-//       // console.log(TimeComponents);
-//       // console.log(finishTime - currentTime);
-//     }, 1000);
-// },
-
-// }
-
-// CountdownTimer.start();
-
-// function pad(value) {
-//   return String(value).padStart(2, '0');
-// }
-
-// function getTimeComponents(time) {
-//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-//   const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-//   return { days, hours, mins, secs };
-// }
-
-
-// const fieldDays = document.querySelector('#timer-1 [data-value="days"]');
-// const fieldHours = document.querySelector('#timer-1 [data-value="hours"]');
-// const fieldMins = document.querySelector('#timer-1 [data-value="mins"]');
-// const fieldSecs = document.querySelector('#timer-1 [data-value="secs"]');
-
-// function updateClockface({ days, hours, mins, secs }) {
-//   fieldDays.textContent = `${days}`;
-//   fieldHours.textContent = `${hours}`;
-//   fieldMins.textContent = `${mins}`;
-//   fieldSecs.textContent = `${secs}`;
-// }
-
 
